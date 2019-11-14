9dfdf71458313a51de09af7f0d4142ac71b801d2
unknown <elvinlkw@hotmail.com>
2019-11-14 15:06:07 -0500

    implement basic functionality and ui

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 var formSubmit = document.querySelector("#form-id");
+var showbar = document.querySelector("#show-input");
+var input = document.querySelector("input");
+var list = [];
+var listCount = 0;
+
+// if(list.length === 0){
+//     const message = document.createElement("h4");
+//     message.textContent = "your to-do list is empty";
+//     message.classList.add("message");
+//     document.querySelector("#todo-container").append(message);
+// }
 
 formSubmit.addEventListener("submit", function(e){
     e.preventDefault();
-    var inputText = document.querySelector("input").value;
-
-    var todo = document.createElement("li");
+    let inputText = input.value;
+    let todo = document.createElement("li");
     todo.textContent = inputText;
+    todo.classList.add("item");
+    if(listCount % 2 === 0){todo.style.backgroundColor = "lightgray";}
+    else{todo.style.backgroundColor = "#eee";}
     document.querySelector("ul").append(todo);
     document.querySelector("input").value = "";
+    listCount += 1;
+});
+
+showbar.addEventListener("click", function(){
+    input.classList.toggle("hidden");
 });
