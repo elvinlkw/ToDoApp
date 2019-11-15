1be49150d2bd7a4c8a666854ba14e1937fb7571c
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2019-11-15 00:07:50 -0500

    added jquery

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-var formSubmit = document.querySelector("#form-id");
-var showbar = document.querySelector("#show-input");
-var input = document.querySelector("input");
-var list = [];
-var listCount = 0;
+$(document).ready(function(){
+    var $formSubmit = $("#form-id");
+    var $showbar = $("#show-input");
+    var $input = $("input");
+    var list = [];
+    var listCount = 0;
 
-formSubmit.addEventListener("submit", function(e){
-    e.preventDefault();
-    list.push(input.value);
-    let inputText = input.value;
-    let todo = document.createElement("li");
-    todo.textContent = inputText;
-    todo.classList.add("item");
-    if(listCount % 2 === 0){todo.style.backgroundColor = "lightgray";}
-    else{todo.style.backgroundColor = "#eee";}
-    document.querySelector("ul").append(todo);
-    document.querySelector("input").value = "";
-    listCount += 1;
+    $formSubmit.submit(function(e){
+        e.preventDefault();
+        let background;
+        let inputText = $input.val();
+        list.push(inputText);
+
+        if(listCount % 2 === 0){background = "#eee";}
+        else{background = "lightgray";}
+
+        $('#todos').append(addList(inputText));
+        $('.item:last-child').css(
+            "background-color", `${background}`);
+        $input.val("");
+        listCount += 1;
+    });
+
+    $showbar.click(function(){
+        $input.toggleClass("hidden");
+    });
 });
 
-showbar.addEventListener("click", function(){
-    input.classList.toggle("hidden");
-});
+function addList(todo){
+    return (
+        `<li class="item">${todo}</li>`
+    );
+}
